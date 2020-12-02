7e81d7b0b54e8f8a28b53da75bc59a22c8502715
Mario Kozjak <kozjakm1@gmail.com>
2020-12-02 20:06:55 +0100

    remove ts source files

--- src/controllers/hello.ts
+++
@@ -1,6 +0,0 @@
-import * as e from 'express'
-
-export function hi(req: e.Request, res: e.Response) {
-	console.log(req.path)
-	res.send('Hello!')
-}
--- src/types/videoshow.d.ts
+++
@@ -1,3 +0,0 @@
-declare module 'videoshow' {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-}
