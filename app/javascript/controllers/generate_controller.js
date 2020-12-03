769ac9d425d0b2b95f6c686c2f15b666acc968fb
Mario Kozjak <kozjakm1@gmail.com>
2020-12-03 21:15:12 +0100

    add initial file upload

--- app/javascript/controllers/generate_controller.js
+++ app/javascript/controllers/generate_controller.js
@@ -1,7 +1,19 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-    async addFile() {
-        console.log("stimulus!")
+    connect() {
+        this.count = 0
+    }
+
+    async initFile() {
+        this.element.ownerDocument.getElementById("fileinput").click()
+    }
+
+    async selected(event) {
+        this.count++
+
+        // remove initial "Add your files" section
+        if (this.count === 1)
+            this.element.ownerDocument.getElementById("initial").remove()
     }
 }
